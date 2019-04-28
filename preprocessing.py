68661fe5e965cbb129a4df213afe10f29c03922b
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2019-04-28 18:57:30 +0200

    correction preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -36,20 +36,18 @@ dfs_responses = np.array([["responses fiscalite", df_resp_fis], ["responses demo
 s = FeaturesExtractor()
 def extract_features():
     for k in range(4):
-
         ids_questions = get_ids_open_reponses(dfs[k,1])
-        ids_auth = set(dfs_responses[k,1]['authorId'].values)
-
+        ids_auth = list(set(dfs_responses[k,1]['authorId'].values))
         responses = (dfs_responses[k,1][:].formattedValue.values.tolist())
-
         # Extract embeddings for sentences
         features = np.zeros((len(ids_auth), 300*len(ids_questions)))
         for i in range(len(ids_auth)) :
             for j in range(len(ids_questions)) :
-                features[i][300*j:300*(j+1)] = s.get_features(dfs_responses[k,1][dfs_responses[k,1]['authorId','questionId'] == ids_auth[i],ids_questions[j]])
-
-
-
+                response_unique = dfs_responses[k,1][dfs_responses[k,1]['authorId'] == ids_auth[i]][dfs_responses[k,              1][dfs_responses[k,1]['authorId'] == ids_auth[i]]['questionId'] == ids_questions[j]].formattedValue.values.tolist()
+                if (len(response_unique) > 0) :
+                    features[i][300*j:300*(j+1)] = s.get_features(response_unique[0])
+                else:
+                    features[i][300*j:300*(j+1)] = [0.]*300
         np.savetxt(dfs_responses[k,0,]+'_all_questions.tsv', features, delimiter='\t')
         #responses_samples = [responses[i] for i in samples_id]
         #with open('labels_'+dfs_responses[k,0]+'_all_questions.tsv', 'w') as f:
